21bd97b026d6c8a8887a871ef2813e3287dd059b
Mai <jea989@gmail.com>
2023-05-04 16:16:29 -0400

    fix

--- gen_usa.sh
+++ gen_usa.sh
@@ -5,13 +5,13 @@ set -uex
 areas=(alabama alaska arizona arkansas california colorado connecticut delaware district-of-columbia florida georgia hawaii idaho illinois indiana iowa kansas kentucky louisiana maine maryland massachusetts michigan minnesota mississippi missouri montana nebraska nevada new-hampshire new-jersey new-mexico new-york north-carolina north-dakota ohio oklahoma oregon pennsylvania puerto-rico rhode-island south-carolina south-dakota tennessee texas us-virgin-islands utah vermont virginia washington west-virginia wisconsin wyoming)
 
 
-mkdir out
-mkdir work
+[ ! -d out ] && mkdir out
+[ ! -d work ] && mkdir work
 cd work
 
 for a in ${areas[@]}; do 
   echo STARTING: $a
-  wget https://download.geofabrik.de/north-america/us/${a}-latest.osm.pbf
+  wget -c https://download.geofabrik.de/north-america/us/${a}-latest.osm.pbf
   mkdir $a
   cd $a
   mkgmap-splitter --keep-complete=true ../${a}-latest.osm.pbf
